05cf6867b5eb05fe7ffc88d8db422cee31410716
jfabian8371 <126533662+jfabian8371@users.noreply.github.com>
2024-11-04 21:39:51 -0800

    Update Health.cs

--- Health.cs
+++ Health.cs
@@ -7,8 +7,10 @@ public class Health : MonoBehaviour
     [Header("Attributes")]
     [SerializeField] private int hitPoints = 2;
     [SerializeField] private int currencyWorth = 50;
+    [SerializeField] private AudioClip death_sound;
 
     private bool isDestroyed = false;//want false first for the if loop
+
 
     public void TakeDamage(int damage)
     {
@@ -19,7 +21,9 @@ public class Health : MonoBehaviour
             StrawSpawner.onStrawDestroy.Invoke();//tell spawner it is destoryed
             LevelManager.main.IncreaseCurrency(50);//add 50 gold everytime an enemy is destroy
             isDestroyed = true;
+            AudioSource.PlayClipAtPoint(death_sound, transform.position, 1f);
             Destroy(gameObject);
+           
         }
     }
 }
