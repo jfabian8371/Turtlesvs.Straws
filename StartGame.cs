9bad6a275838222aa29b54f5507763425fcd1893
jfabian8371 <126533662+jfabian8371@users.noreply.github.com>
2024-11-04 21:40:34 -0800

    Update StartGame.cs

--- StartGame.cs
+++ StartGame.cs
@@ -5,10 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour
 {
+    [SerializeField] private AudioClip clickSound;
     public string LevelName;//scene for game
+    
 
     public void LoadLevel()
     {
+        AudioSource.PlayClipAtPoint(clickSound, transform.position, 1f);
         SceneManager.LoadScene(LevelName);
     }
 }
