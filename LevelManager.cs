0f256f3b9059df859ac314817021491f470630d9
jfabian8371 <126533662+jfabian8371@users.noreply.github.com>
2024-11-04 21:40:03 -0800

    Update LevelManager.cs

--- LevelManager.cs
+++ LevelManager.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
 
     public static LevelManager main;//have it public so others can use
 
-
+    //[Header("References")]
     public Transform startPoint;
     public Transform[] path;
 
+//[Header("Attributes")]
     public int currency;
+    private int strawsAtEnd = 0;
+    private int strawsToEndPath = 3;
     private void Awake()
     {
         main = this;
@@ -40,6 +44,21 @@ public class LevelManager : MonoBehaviour
             Debug.Log("Not enough gold to buy");
             return false;//if there isn;t enough to buy the tower return false
         }
+    }
+
+    public void RegisterStrawAtEnd()
+    {
+        strawsAtEnd++;//means a straw made it
+        if (strawsAtEnd >= strawsToEndPath)
+        {
+            EndGame();
+        }
+    }
+
+    private void EndGame()
+    {
+        Debug.Log("Game over!");
+        SceneManager.LoadScene("EndGameScene");//this will just switch the game to end
     }
 
 }
